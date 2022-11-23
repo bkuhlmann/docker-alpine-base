b953bb48b2f7e0cd7cc074c9bf446b33eb3995e7
Brooke Kuhlmann <brooke@alchemists.io>
2022-11-22 19:38:02 -0700

    Fixed shell loader to not exit if profile is not found
    Ensures you can still launch into a local console even if this file
    doesn't exist while still displaying a message as a warning and
    reminder.

--- .config/docker-alpine-base/shell_loader.sh
+++ .config/docker-alpine-base/shell_loader.sh
@@ -10,6 +10,5 @@ profile_path="/workspace/.config/profile.sh"
 if [[ -r "$profile_path" ]]; then
   source "$profile_path"
 else
-  printf "%s\n" "File not found or readable: $profile_path."
-  exit 1
+  printf "%s\n" "Profile skipped. File not found or readable: $profile_path."
 fi
